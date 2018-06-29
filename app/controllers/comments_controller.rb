affa43637fe61cced9db785cefc0d7f75af534f9
AnabolicAction <itw9622@gmail.com>
2018-06-29 08:28:31 +0000

    rails ajax 구현

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,23 @@
 class CommentsController < ApplicationController
-  
+
   def create
     @comment = Post.find(params[:post_id]).comments.new(comment_params)
-    @comment.save
+    if @comment.save
+    respond_to do |format|
+      format.html { redirect_to :back}
+      format.js { render 'create_temp'}
+    end
+  else
     redirect_to :back
+  end
+end
+def destroy
+    @comment=Comment.find(params[:comment_id])
+    @comment.destroy
+    respond_to do |format|
+      format.html {redirect_to :back}
+      format.js {}
+    end
   end
 
   private
